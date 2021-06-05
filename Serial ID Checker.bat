c0a1592e821e31e3a5c00f501c1172ba8acdf0c7
Detroitt <83370815+Detroitt@users.noreply.github.com>
2021-06-05 12:33:00 -0300

    Add files via upload

---
+++ Serial ID Checker.bat
@@ -0,0 +1,55 @@
+@echo off
+color 3
+echo "  /$$$$$$                      /$$           /$$                            
+echo " /$$__  $$                    |__/          | $$                            
+echo "| $$  \__/  /$$$$$$   /$$$$$$  /$$  /$$$$$$ | $$                            
+echo "|  $$$$$$  /$$__  $$ /$$__  $$| $$ |____  $$| $$                           
+echo " \____  $$| $$$$$$$$| $$  \__/| $$  /$$$$$$$| $$                            
+echo " /$$  \ $$| $$_____/| $$      | $$ /$$__  $$| $$                           
+echo "|  $$$$$$/|  $$$$$$$| $$      | $$|  $$$$$$$| $$                           
+echo " \______/  \_______/|__/      |__/ \_______/|__/           
+echo "=================================================                 
+echo "                                                                           
+echo "                                                                           
+echo "                                                                       
+echo "           /$$                           /$$                                
+echo "          | $$                          | $$                                
+echo "  /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$   /$$$$$$      
+echo " /$$_____/| $$__  $$ /$$__  $$ /$$_____/| $$  /$$/ /$$__  $$ /$$__  $$      
+echo "| $$      | $$  \ $$| $$$$$$$$| $$      | $$$$$$/ | $$$$$$$$| $$  \__/     
+echo "| $$      | $$  | $$| $$_____/| $$      | $$_  $$ | $$_____/| $$            
+echo "|  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$$| $$            
+echo " \_______/|__/  |__/ \_______/ \_______/|__/  \__/ \_______/|__/   
+echo "========================================================================   
+echo "
+echo "
+echo "                            PRESS ANY KEY!
+pause>nul
+
+color 0                                                                                        
+
+echo Baseboard
+
+wmic baseboard get serialnumber
+
+echo Bios
+
+wmic bios get serialnumber
+
+echo CPU
+
+wmic cpu get serialnumber
+
+echo Diskdrive
+
+wmic diskdrive get serialnumber
+
+echo Baseboard
+
+wmic baseboard get manufacturer
+
+echo Mac Address
+
+getmac
+
+pause>nul
